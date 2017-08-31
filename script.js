a43d78a73860faaf79cb8cc815f40a21405ddd9c
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-08-31 23:45:30 +0200

    Web panel rewritten to react

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
-var App = {
-    container: [],
-    canvas: null,
-    file: null,
-    colorPage: document.getElementsByClassName('js-page-color')[0],
-    imagePage: document.getElementsByClassName('js-page-image')[0],
-    inputH: document.getElementsByClassName('js-color-h')[0],
-    inputS: document.getElementsByClassName('js-color-s')[0],
-    inputL: document.getElementsByClassName('js-color-l')[0],
-    colorPreview: document.getElementsByClassName('js-color-example')[0],
+class Utils {
+    static getColor = ({h, s, l}) => "hsl(" + h + ", " + s + "%, " + l + "%)";
     
-    init: function() {
-        this.initSocket();
-        
-        this.file = document.getElementsByClassName('js-input')[0];
-        this.file.addEventListener('change', this.handleImageChange.bind(this));
-        
-        this.canvas = document.getElementsByClassName('js-image')[0];
-        this.canvas.addEventListener('click', this.handleLineSelect.bind(this));
-        
-        this.colorPage.style.display = 'block';
-        this.imagePage.style.display = 'none';
-        
-        this.handleColorChange();
-    },
-    
-    getColor: function() {
-        const h = this.inputH.value;
-        const s = this.inputS.value;
-        const l = this.inputL.value;
-        
-        return "hsl(" + h + ", " + s + "%, " + l + "%)";
-    },
-    
-    handleColorChange: function(e) {
-        this.colorPreview.style.backgroundColor = this.getColor();
-    },
-    
-    handleChangePage: function(page) {
-        switch(page){
-            case 'color':
-                this.colorPage.style.display = 'block';
-                this.imagePage.style.display = 'none';
-                break;
-            case 'image':
-                this.colorPage.style.display = 'none';
-                this.imagePage.style.display = 'block';
-                break;
-        }
-    },
-    
-    handleLineSelect: function (e) {
-        this.countAvg(this.canvas.getContext('2d'), e.layerY);
-    },
-    
-    initSocket: function() {
-        this.socket = io(config.uri);
-        
-        this.socket.on('connect', () => {
-            this.socket.emit('init');
+    static rgbToHex = (r, g, b) => {
+        var bin = r << 16 | g << 8 | b;
+        return (function(h){
+            return new Array(7-h.length).join("0")+h
+        })(bin.toString(16).toUpperCase())
+    }
+}
+class Info extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {container} = this.props;
+        return <div>
+            {container.map(item => {
+                return <div>{item.id} {item.title}</div>
+            })}
+        </div>
+    }
+}
 
-            this.socket.on('initSuccess', response => {
-                this.container.push(response);
-            });
-        });
-    },
-    
-    countAvg: function(ctx, line) {
+class ColorInput extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            color: {h: 0, s: 50, l: 50}
+        }
+    }
+    handleColorChange = (e, item) => {
+        this.setState({
+            color: Object.assign({}, this.state.color, {
+                [item]: e.target.value
+            })
+        })
+    }
+    render() {
+        const {color} = this.state;
+        const {setColor} = this.props;
+        return <div>
+            <div style={{backgroundColor: Utils.getColor(color)}} className="color-example js-color-example input"></div>
+            <div className="input--slider">
+                <input value={color.h} onInput={e => this.handleColorChange(e, 'h')} className="input js-color-h" type="range" min="0" max="360" step="1" />
+            </div>
+            <div className="input--slider">
+                <input value={color.s} onInput={e => this.handleColorChange(e, 's')} className="input js-color-s" type="range" min="0" max="100" step="1" />
+            </div>
+            <div className="input--slider">
+                <input value={color.l} onInput={e => this.handleColorChange(e, 'l')} className="input js-color-l" type="range" min="0" max="100" step="1" />
+            </div>
+            <button className="input btn" onClick={() => setColor([Utils.getColor(color)])}>Set</button>
+        </div>
+    }
+}
+
+class ImageInput extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    handleLineSelect = (e) => {
+        this.countAvg(this.canvas.getContext('2d'), e.layerY);
+    }
+    handleImageChange = (e) => {
+        var ctx = this.canvas.getContext('2d');
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            const url = e.target.result;
+            this.prev.src = url;
+            this.prev.onload = (e) => {
+                this.canvas.width = this.prev.offsetWidth;
+                this.canvas.height = this.prev.offsetHeight;
+                ctx.drawImage(this.prev, 0, 0, this.prev.offsetWidth, this.prev.offsetHeight);
+                this.countAvg(ctx);
+            }
+        }.bind(this);
+
+        reader.readAsDataURL(e.target.files[0]);
+        
+    }
+    countAvg = (ctx, line) => {
+        const {setColor} = this.props;
         // var x = 0;
         // setInterval(() => {
             var map = [];
@@ -82,64 +99,105 @@ var App = {
                 avg[0] /= offset;
                 avg[1] /= offset;
                 avg[2] /= offset;
-                map.push('#' + this.rgbToHex(parseInt(avg[0]), parseInt(avg[1]), parseInt(avg[2])));
+                map.push('#' + Utils.rgbToHex(parseInt(avg[0]), parseInt(avg[1]), parseInt(avg[2])));
             }
             
-            this.handleSet(map);
+            setColor(map);
             // x++;
         // }, 1);
-    },
-    
-    handleImageChange: function(e) {
-        var ctx = this.canvas.getContext('2d');
-        var prev = document.getElementsByClassName('js-image-preview')[0];
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            const url = e.target.result;
-            prev.src = url;
-            prev.onload = (e) => {
-                this.canvas.width = prev.offsetWidth;
-                this.canvas.height = prev.offsetHeight;
-                ctx.drawImage(prev, 0, 0, prev.offsetWidth, prev.offsetHeight);
-                this.countAvg(ctx);
-            }
-        }.bind(this);
-
-        reader.readAsDataURL(this.file.files[0]);
-        
-    },
-    
-    rgbToHex: function(r, g, b) {
-        var bin = r << 16 | g << 8 | b;
-        return (function(h){
-            return new Array(7-h.length).join("0")+h
-        })(bin.toString(16).toUpperCase())
-    },
-    
-    hexToRgb: function(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    },
-    
-    handleSet: function(map) {
-        // const col = document.getElementsByClassName('js-color')[0].value;
-        this.socket.emit('set', {
-            id: 0,
-            color: map
-        });
-    },
-    
-    handleSetColor: function() {
-        this.socket.emit('set', {
-            id: 0,
-            color: [this.getColor()]
-        });
+    }
+    render() {
+        return <div>
+            <input type="file" className="js-input inputfile" id="file" onChange={this.handleImageChange}/>
+            <label className="input" htmlFor="file">Choose a picture</label>
+            
+            <canvas ref={c => this.canvas = c} className="image js-image"></canvas>
+            <img ref={c => this.prev = c} className="js-image-preview image-preview" onClick={this.handleLineSelect} />
+        </div>
     }
 }
 
-App.init();
+class Content extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        const {setColor, container} = this.props;
+        
+        switch(this.props.page){
+            case 'color':
+                return <ColorInput setColor={setColor}/>;
+            case 'image':
+                return <ImageInput setColor={setColor} />;
+            case 'info':
+                return <Info container={container} />;
+            default:
+                return false;
+        }
+    }
+}
+
+class Nav extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        const {changePage} = this.props;
+        return <div className="nav">
+            <a onClick={() => changePage('color')} className="nav__item">Color</a>
+            <a onClick={() => changePage('image')} className="nav__item">Image</a>
+            <a onClick={() => changePage('info')} className="nav__item">Info</a>
+        </div>
+    }
+}
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.initSocket();
+        
+        this.state = {
+            page: 'color',
+            container: []
+        }
+    }
+    
+    changePage = (page) => {
+        this.setState({page});
+    }
+    
+    initSocket = () => {
+        this.socket = io(config.uri);
+        
+        this.socket.on('connect', () => {
+            this.socket.emit('init');
+
+            this.socket.on('initSuccess', response => {
+                this.setState(prev => ({
+                    container: [].concat(prev.container, response)
+                }));
+            });
+        });
+    }
+    
+    handleSet = (color) => {
+        this.socket.emit('set', {
+            id: 0,
+            color: [color]
+        });
+    }
+    
+    render() {
+        const {page, container} = this.state;
+        return <div>
+            <Nav changePage={this.changePage} />
+            <Content setColor={this.handleSet} page={page} container={container} />
+        </div>;
+    }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
